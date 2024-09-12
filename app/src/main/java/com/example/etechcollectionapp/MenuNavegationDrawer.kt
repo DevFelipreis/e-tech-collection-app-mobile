a090ba1b08365c89885cd8ffd5ba852dfc36062a
DevFelipreis <lfreis.contato@gmail.com>
2024-09-11 23:53:08 -0300

    change color button

--- app/src/main/java/com/example/etechcollectionapp/MenuNavegationDrawer.kt
+++ app/src/main/java/com/example/etechcollectionapp/MenuNavegationDrawer.kt
@@ -1,8 +1,12 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,14 +18,33 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import com.example.e_techcollectionapp.ui.theme.DarkGreen
+import com.example.etechcollectionapp.R
 
 @Composable
 fun DrawerContent(onCloseDrawer: () -> Unit) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .width(300.dp)
+            .background(color = DarkGreen)
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.perfil),
+            contentDescription = "Perfil",
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp)
+        )
         Text(
-            "Menu Navegação Drawer",
-            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(8.dp)
+            "Meu Nome",
+            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color.White),
+            modifier = Modifier.padding(start = 8.dp)
+        )
+        Text(
+            "Minha Conta",
+            style = TextStyle(fontSize = 21.sp, fontWeight = FontWeight.Normal, color = Color.White),
+            modifier = Modifier.padding(start = 8.dp)
         )
         Spacer(modifier = Modifier.height(16.dp))
         DrawerOption("Início", Icons.Filled.Home)
@@ -30,18 +53,34 @@ fun DrawerContent(onCloseDrawer: () -> Unit) {
         DrawerOption("Cadastrar locais de coleta", Icons.Filled.Add)
         DrawerOption("Meus Descartes", Icons.Filled.Delete)
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onCloseDrawer, modifier = Modifier.fillMaxWidth()) {
-            Text("Sair")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Button(
+                onClick = onCloseDrawer,
+                modifier = Modifier.height(32.dp)
+            ) {
+                Text("Sair")
+            }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = R.drawable.logo2),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
     }
 }
 
 @Composable
 fun DrawerOption(text: String, icon: ImageVector) {
     Row(modifier = Modifier.padding(8.dp)) {
-        Icon(imageVector = icon, contentDescription = text, modifier = Modifier.size(24.dp))
+        Icon(imageVector = icon, contentDescription = text, tint = Color.White, modifier = Modifier.size(24.dp))
         Spacer(modifier = Modifier.width(16.dp))
-        Text(text, style = TextStyle(fontSize = 16.sp))
+        Text(text, style = TextStyle(fontSize = 16.sp, color = Color.White))
     }
 }
 
